4c3f7a0a33d23f605ed196743113c532bad33f57
yuzhe <953659912@qq.com>
2016-08-04 21:17:45 +0800

    8_4

---
+++ 汉诺塔.cpp
@@ -0,0 +1,33 @@
+#include<cstdlib>
+#include<iostream>
+
+
+//汉诺塔，看错最简洁的方式进行操作，
+//递归，就是看作一个和另一个，进行递归的操作就可以了
+//递归的精髓就是我每一步都要依赖上面的一步
+
+void hannuo(int m,char a,char b,char c)
+{
+	if (m < 1)
+	{
+		return;
+	}
+	else
+	{
+		hannuo(m - 1, a, c, b);
+		std::cout << a << "->" << c << std::endl;
+		hannuo(m - 1, b, a, c);
+		
+	}
+	
+}
+
+int main()
+{
+	int m(0);
+	std::cout << "输入汉诺塔层数" << std::endl;
+	std::cin >> m;
+	hannuo(m,'a','b','c');
+	system("pause");
+	return 0;
+}
